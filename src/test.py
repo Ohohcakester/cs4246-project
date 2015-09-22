95eabc03fa14ec68601ca08b264ec1f217f5f848
ndt93 <thiennguyen1993@gmail.com>
2015-09-22 15:01:38 +0800

    Experiment experiment

--- src/test.py
+++ src/test.py
@@ -28,20 +28,40 @@ def SVGP(X, Y):
     return m
 
 def GP(X, Y):
-    m = GPy.models.GPRegression(X, Y, GPy.kern.RBF(1, 0.7, 0.2))
+    if Y.ndim != 2:
+        Y = Y[:, None]
+    m = GPy.models.GPRegression(X, Y, GPy.kern.Matern52(1, 0.5, 0.2))
     m.optimize(messages=True, max_f_eval=1000)
     return m
 
-data = pd.read_csv('../data/trainingData.csv')
-data_filtered = data[['USERID', 'LONGITUDE', 'LATITUDE', 'FLOOR', 'BUILDINGID', 'TIMESTAMP']]
+def readData():
+    """
+    data = pd.read_csv('../data/trainingData.csv')
+    data_filtered = data[['USERID', 'LONGITUDE', 'LATITUDE', 'FLOOR', 'BUILDINGID', 'TIMESTAMP']]
 
-first_user = data_filtered[data_filtered.USERID == 1]
-first_user = first_user[first_user.TIMESTAMP <= 1370900000]
-first_user.drop('USERID', axis=1)
+    first_user = data_filtered[data_filtered.USERID == 1]
+    first_user = first_user[first_user.TIMESTAMP <= 1370900000]
+    first_user.drop('USERID', axis=1)
 
-X = np.array(first_user['TIMESTAMP'][:, None]).astype(float)
-X -= np.min(X)
-Y = np.array(first_user[['LONGITUDE', 'LATITUDE']])
+    first_user.to_csv('first_user.csv', index=None)
+
+    X = np.array(first_user['TIMESTAMP'][:, None]).astype(float)
+    X -= np.min(X)
+    Y = np.array(first_user[['LONGITUDE', 'LATITUDE']])
+    """
+    first_user = pd.read_csv('first_user.csv')
+    X = np.array(first_user['TIMESTAMP'][:, None]).astype(float)
+    X -= np.min(X)
+    Y = np.array(first_user[['LONGITUDE', 'LATITUDE']])
+
+    return (X, Y)
+
+X, Y = readData()
+
+N = len(X)
+Y1 = np.sin(6*X) + 0.1*np.random.randn(N, 1)
+Y2 = np.sin(3*X) + 0.1*np.random.randn(N, 1)
+Y = np.hstack((Y1, Y2))
 
 fig1 = plt.figure(num=1)
 plt.plot(X, Y[:, 0], 'bx', alpha=0.2)
@@ -49,5 +69,5 @@ plt.xlabel('Time')
 plt.ylabel('LONGITUDE')
 plt.title('SVI LONGITUDE prediction with data')
 
-m = GP(X, Y)
+m = SVGP(X, Y)
 _ = m.plot(which_data_ycols=[0], plot_limits=(X.min(), X.max()), fignum=1)
