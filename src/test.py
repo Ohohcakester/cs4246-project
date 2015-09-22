3457b9c905561077db949c3e9836c46ded941aef
huixian <lynnette-ng@outlook.com>
2015-09-22 16:23:01 +0800

    Split Data

--- src/test.py
+++ src/test.py
@@ -5,17 +5,19 @@ from matplotlib import pyplot as plt
 import climin
 
 def SVGP(X, Y):
+    if Y.ndim != 2:
+        Y = Y[:, None]
     Z = np.random.rand(20, 1)
     batchsize = 20
     m = GPy.core.SVGP(X, Y, Z,
-                      GPy.kern.RBF(1) + GPy.kern.White(1),
-                      GPy.likelihoods.Gaussian(), batchsize=batchsize)
-    m.kern.white.variance = 1e-5
-    m.kern.white.fix()
+            GPy.kern.Matern52(1),
+            GPy.likelihoods.Gaussian(), batchsize=batchsize)
+    #m.kern.white.variance = 1e-5
+    #m.kern.white.fix()
 
     opt = climin.Adadelta(m.optimizer_array,
-                          m.stochastic_grad,
-                          step_rate=0.2, momentum=0.9)
+            m.stochastic_grad,
+            step_rate=0.2, momentum=0.9)
     def callback(i):
         print m.log_likelihood(), "\r",
         if i['n_iter'] > 5000:
@@ -30,34 +32,32 @@ def SVGP(X, Y):
 def GP(X, Y):
     if Y.ndim != 2:
         Y = Y[:, None]
-    kernel = GPy.kern.Matern52(1)
+    kernel = GPy.kern.RBF(1, 4, 0.2)
     m = GPy.models.GPRegression(X, Y, kernel)
     m.optimize(messages=True, max_f_eval=1000)
     return m
 
-def readData():
-    """
+def splitData():
     data = pd.read_csv('../data/trainingData.csv')
     data_filtered = data[['USERID', 'LONGITUDE', 'LATITUDE', 'FLOOR', 'BUILDINGID', 'TIMESTAMP']]
 
-    first_user = data_filtered[data_filtered.USERID == 1]
-    first_user = first_user[first_user.TIMESTAMP <= 1370900000]
+    first_user = data_filtered[data_filtered.USERID == 2]
+    #first_user = first_user[first_user.TIMESTAMP <= 1370900000]
     first_user.drop('USERID', axis=1)
 
-    first_user.to_csv('first_user.csv', index=None)
+    first_user.to_csv('second_user.csv', index=None)
+    
+def readData(filename):
+    user = pd.read_csv(filename)
 
-    X = np.array(first_user['TIMESTAMP'][:, None]).astype(float)
+    X = np.array(user['TIMESTAMP'][:, None]).astype(float)
     X -= np.min(X)
-    Y = np.array(first_user[['LONGITUDE', 'LATITUDE']])
-    """
-    first_user = pd.read_csv('first_user.csv')
-    X = np.array(first_user['TIMESTAMP'][:, None]).astype(float)
-    X -= np.min(X)
-    Y = np.array(first_user[['LONGITUDE', 'LATITUDE']])
+    Y = np.array(user[['LONGITUDE', 'LATITUDE']])
 
     return (X, Y)
 
-X, Y = readData()
+
+X, Y = readData('second_user.csv')
 
 '''
 N = len(X)
@@ -67,10 +67,10 @@ Y = np.hstack((Y1, Y2))
 '''
 
 fig1 = plt.figure(num=1)
-plt.plot(X, Y[:, 0], 'bx', alpha=0.2)
+plt.plot(X, Y[:, 1], 'bx', alpha=0.2)
 plt.xlabel('Time')
 plt.ylabel('LONGITUDE')
 plt.title('SVI LONGITUDE prediction with data')
 
-m = GP(X, Y[:, 0])
+m = GP(X, Y[:, 1])
 _ = m.plot(which_data_ycols=[0], plot_limits=(X.min(), X.max()), fignum=1)
