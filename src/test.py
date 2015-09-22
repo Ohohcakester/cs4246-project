dc3d61db68f1558b23b787f7272c90cba65d5cae
ndt93 <thiennguyen1993@gmail.com>
2015-09-22 12:31:32 +0800

    Experiment

--- src/test.py
+++ src/test.py
@@ -1,30 +1,23 @@
-#!/usr/bin/python
-
 import pandas as pd 
 import numpy as np
 import GPy
 from matplotlib import pyplot as plt
 import climin
 
-import sys
-
-data = pd.read_csv('data/trainingData.csv')
-data_filtered = data[['USERID', 'LONGTITUDE', 'LATITUDE', 'FLOOR', 'BUILDINGID', 'TIMESTAMP']]
+data = pd.read_csv('../data/trainingData.csv')
+data_filtered = data[['USERID', 'LONGITUDE', 'LATITUDE', 'FLOOR', 'BUILDINGID', 'TIMESTAMP']]
 
 first_user = data_filtered[data_filtered.USERID == 1]
 first_user.drop('USERID', axis=1)
 
-X = np.arrayfirst_user['TIMESTAMP'])[:, None]
-Y = np.array(first_user[['LONGTITUDE', 'LATITUDE', 'FLOOR']])
-Y_1d = np.transpose(Y[:0])
+X = np.array(first_user['TIMESTAMP'][:, None]).astype(float)
+Y = np.array(first_user[['LONGITUDE', 'LATITUDE', 'FLOOR']])
 
-Z = np.random.rand(20,1)
-
+Z = np.random.rand(20, 1)
 batchsize = 10
 m = GPy.core.SVGP(X, Y, Z, GPy.kern.RBF(1) + GPy.kern.White(1), GPy.likelihoods.Gaussian(), batchsize=batchsize)
 m.kern.white.variance = 1e-5
 m.kern.white.fix()
-
 
 opt = climin.Adadelta(m.optimizer_array, m.stochastic_grad, step_rate=0.2, momentum=0.9)
 def callback(i):
@@ -35,3 +28,9 @@ def callback(i):
     return False
 info = opt.minimize_until(callback)
 
+fig1 = plt.figure(num=1)
+plt.plot(X, Y[:, 0], 'bx', alpha=0.2)
+plt.xlabel('Time')
+plt.ylabel('LONGITUDE')
+plt.title('SVI LONGITUDE prediction with data')
+_ = m.plot(which_data_ycols=[0], plot_limits=(X.min(), X.max()), fignum=1)
