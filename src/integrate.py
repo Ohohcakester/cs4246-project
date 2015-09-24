19aa52e061a8eedaff7c2d76b1666bfa21d1c1e5
Ohohcakester <ohohgit@gmail.com>
2015-09-24 13:09:37 +0800

    add comments to integrate.py

--- src/integrate.py
+++ src/integrate.py
@@ -1,6 +1,8 @@
 from scipy import integrate
 import numpy as np
 
+# f is a function (probability distribution) of x, y.
+# Finds area under a circle at (cx,cy), radius r.
 def circleIntegrate(cx, cy, r, f):
     yTop = lambda x : cy + (r*r - (x - cx)*(x - cx))**0.5
     yBot = lambda x : cy - (r*r - (x - cx)*(x - cx))**0.5
@@ -8,7 +10,9 @@ def circleIntegrate(cx, cy, r, f):
     res, err = integrate.dblquad(f, cx - r, cy + r, yBot, yTop)
     print 'Result: ' + str(res)
     print 'Error: ' + str(err)
-    
+
+# f is a function (probability distribution) of x, y, z.
+# Integrates over a cylinder at (cx,cy), radius r, from z = zMin to z = zMax
 def cylinderIntegrate(cx, cy, r, zMin, zMax, f):
     yTop = lambda x : cy + (r*r - (x - cx)*(x - cx))**0.5
     yBot = lambda x : cy - (r*r - (x - cx)*(x - cx))**0.5
@@ -20,6 +24,8 @@ def cylinderIntegrate(cx, cy, r, zMin, zMax, f):
     print 'Error: ' + str(err)
 
 
-def f(x,y,z):
-    return 1
-cylinderIntegrate(0,0,1,-1,1,f)
+# Testing code
+if __name__ == '__main__':
+    def f(x,y,z):
+        return 1
+    cylinderIntegrate(0,0,1,-1,1,f)
