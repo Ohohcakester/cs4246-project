41b5d2a49f2af4eb3e595640a7012c31ef1a3cfc
Nathan Azaria <nathanajah@yahoo.com>
2015-10-06 18:51:55 +0800

    Integrate.py update

--- src/integrate.py
+++ src/integrate.py
@@ -7,9 +7,7 @@ def circleIntegrate(cx, cy, r, f):
     yTop = lambda x : cy + (r*r - (x - cx)*(x - cx))**0.5
     yBot = lambda x : cy - (r*r - (x - cx)*(x - cx))**0.5
 
-    res, err = integrate.dblquad(f, cx - r, cy + r, yBot, yTop)
-    print 'Result: ' + str(res)
-    print 'Error: ' + str(err)
+    res, err = integrate.dblquad(f, cx - r, cx + r, yBot, yTop)
 
 # f is a function (probability distribution) of x, y, z.
 # Integrates over a cylinder at (cx,cy), radius r, from z = zMin to z = zMax
@@ -19,9 +17,7 @@ def cylinderIntegrate(cx, cy, r, zMin, zMax, f):
     zTop = lambda x, y : zMax
     zBot = lambda x, y : zMin
     
-    res, err = integrate.tplquad(f, cx - r, cy + r, yBot, yTop, zBot, zTop)
-    print 'Result: ' + str(res)
-    print 'Error: ' + str(err)
+    res, err = integrate.tplquad(f, cx - r, cx + r, yBot, yTop, zBot, zTop)
 
 # f is a function (probability distribution) of x, y, z.
 # Integrates over a cylinder at (cx,cy), radius r, from z = zMin to z = zMax
@@ -31,9 +27,8 @@ def boxIntegrate(cx, cy, r, zMin, zMax, f):
     zTop = lambda x, y : zMax
     zBot = lambda x, y : zMin
     
-    res, err = integrate.tplquad(f, cx - r, cy + r, yBot, yTop, zBot, zTop)
-    print 'Result: ' + str(res)
-    print 'Error: ' + str(err)
+    res, err = integrate.tplquad(f, cx - r, cx + r, yBot, yTop, zBot, zTop)
+    return (res,err)
 
 # Returns a multivariate gaussian pdf using the parameters given.
 # cov_matrix is a 3-d np.array. Should be symmetric, positive definite.
@@ -42,7 +37,7 @@ def multivariateGaussian(mean_x, mean_y, mean_z, cov_matrix):
     inv_matrix = np.linalg.inv(cov_matrix)
     det = np.linalg.det(cov_matrix)
     const = 1 / (((2*np.pi)**3 * det)**0.5)
-    def f(x,y,z):
+    def f(z,y,x):
         v = np.array([[x-mean_x],[y-mean_y],[z-mean_z]])
         return const*np.exp(-0.5*np.dot(np.transpose(v),np.dot(inv_matrix,v)))
     return f
