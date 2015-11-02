9d834d68b5e6f3a1990387867aece3de56449112
ndt93 <thiennguyen1993@gmail.com>
2015-11-02 15:44:29 +0800

    Implement GP interfaces

--- src/bayes.py
+++ src/bayes.py
@@ -1,17 +1,26 @@
 import pandas as pd
-import numpy as np
 
 import GPy
 
-import os
+def regressGP(df):
+    """
+    Regress a GP with data in df.
+    ---
+    df: a DataFrame of ['TIMESTAMP', 'SHORTEST_PATHS']. 'SHORTEST_PATH' should
+        be a series of 3-tuples, each of which contain shortest distances to
+        each of the 3 selected points.
+    ---
+    Return:
+        A GPy model
+    """
 
-'''
-Make the GP part take in a dataframe of [timestamp, user, shortest path] and return a dict of { timestamp: dataframe of [user, mean, var] }
-'''
+    y_cols = ['Y1', 'Y2', 'Y3']
+    for n, col in enumerate(y_cols):
+        df[col] = df['SHORTEST_PATHS'].apply(lambda paths: paths[n])
+    df.drop('SHORTEST_PATHS', axis=1, inplace=True)
 
-def GP(X, Y):
-    if Y.ndim != 2:
-        Y = Y[:, None]
+    X = df['TIMESTAMP'].values.reshape(-1, 1)
+    Y = df[['Y1', 'Y2', 'Y3']].values.astype(float)
 
     kernel = GPy.kern.MLP(1) + GPy.kern.Bias(1)
     m = GPy.models.GPRegression(X, Y, kernel)
@@ -19,18 +28,37 @@ def GP(X, Y):
     m['.*noise'].unconstrain()
     m['.*noise'].fix()
     m.optimize(messages=True, max_f_eval=1000)
+
     return m
 
-def run(df):
-    m = GP(df['timestamp'], df['shortest_path'])
-    mu, var = m.predict(df['timestamp'])
+def predictGP(df):
+    """
+    Predict the mean and variance for each timestamp in a DataFrame, assuming
+    that all data in the DataFrame is regressed in the same series
+    ---
+    df: a DataFrame of ['TIMESTAMP', 'SHORTEST_PATHS']. 'SHORTEST_PATH' should
+        be a series of 3-tuples, each of which contain shortest distances to
+        each of the 3 selected points.
+    ---
+    Return:
+        A DataFrame of ['TIMESTAMPE', 'MU', 'VAR']
+        'MU' and 'VAR' both contains series of 3-tuples
+    """
+    m = regressGP(df)
+    mu, var = m.predict(df['TIMESTAMP'].values.reshape(-1, 1))
+
+    mu = map(tuple, mu)
+    var = map(tuple, var)
+
+    result_df = pd.DataFrame({'TIMESTAMP': df['TIMESTAMP'],
+                              'MU': mu, 'VAR': var})
+    return result_df
 
 if __name__ == '__main__':
-    # Currently assume data formatted already
-    df = pd.read_csv('paths.csv')
-    mu, var = run(df) 
-    df_new = pd.DataFrame({'user': df['user'],
-                            'mu': mu.flatten(),
-                            'var': var.flatten()})
-    userID = df['user'][0]
-    newframe = {userID: df_new}
+    df = pd.read_csv('testGP.csv')
+    df['SHORTEST_PATHS'] = df['SHORTEST_PATHS'].str.split(',')
+
+    userID = df['USER'][0]
+    result = predictGP(df[df['USER'] == userID])
+
+    print result
